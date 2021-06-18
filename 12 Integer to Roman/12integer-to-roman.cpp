bcdb35aed168734e0023c713eddd580a5092078a
dw2008 <tiantianwoah@gmail.com>
2021-06-18 00:20:42 +0000

    Code challenge submission for question: 12 Integer to Roman

---
+++ 12 Integer to Roman/12integer-to-roman.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string romans[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+        int numerals[]={1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        string res;
+        while(num)
+        {
+            for(int i=12;i>=0;i--)
+            {
+                int x = num/numerals[i];
+                if(x)
+                {
+                    while(x--)
+                    {
+                        res += romans[i];
+                    }
+                    num %= numerals[i];
+                    break;
+                }
+            }
+        }   
+        return res;
+    }
+};
