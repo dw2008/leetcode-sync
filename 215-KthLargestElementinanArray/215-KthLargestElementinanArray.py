5b927ceccb886626b385b71df9b283df3168f63b
Daniel Wu <48874007+dw2008@users.noreply.github.com>
2026-09-19 18:04:56 -0500

    File updated at 9/19/2026, 6:04:56 PM

--- 215-KthLargestElementinanArray/215-KthLargestElementinanArray.py
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.py
@@ -1,18 +1,18 @@
-# Last updated: 9/19/2026, 5:58:48 PM
+# Last updated: 9/19/2026, 6:04:56 PM
 1import heapq
 2
 3class Solution:
 4    """
-5    use a reversed min heap? i dont know if that considered sorting lol just pop k times? idk man
-6    """
-7    def findKthLargest(self, nums: list[int], k: int) -> int:
-8        for i in range(len(nums)):
-9            nums[i] = nums[i] * -1
-10            
-11        heapify(nums)
-12        curr = 0
-13        
-14        for i in range(k):
-15            curr = heappop(nums)
+5    push values onto min heap until size of heap is larger than k, then pop the
+6    smallest value. keep repeating this until all numbers are pushed onto the heap,
+7    then take the smallest value from there. it is impossible for the kth smallest
+8    value to be popped out, so this approach works and is faster than just heapify.
+9    """
+10    def findKthLargest(self, nums: list[int], k: int) -> int:
+11        heap = []
+12        for n in nums:
+13            heappush(heap, n)
+14            if(len(heap) > k):
+15                heappop(heap)
 16        
-17        return curr * -1
+17        return heap[0]
